46c8e9444880f7c08d1455eb72bb70c199cd82a7
Rochel Angelo <rochelangelo72@gmail.com>
2017-06-26 11:44:08 -0300

    Imlementando repositorio de aluno

--- GerenciadorDeMatricula/src/main/java/br/edu/garanhuns/ifpe/repositorio/implementacao/RepositorioAluno.java
+++ GerenciadorDeMatricula/src/main/java/br/edu/garanhuns/ifpe/repositorio/implementacao/RepositorioAluno.java
@@ -6,6 +6,7 @@
 package br.edu.garanhuns.ifpe.repositorio.implementacao;
 
 import br.edu.garanhuns.ifpe.entidades.Aluno;
+import br.edu.ifpe.DAO.DaoManagerHiber;
 import br.edu.ifpe.repositorio.comportamentos.RepositorioGenerico;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,51 +15,37 @@ import java.util.List;
  *
  * @author esdra
  */
-public class RepositorioAluno implements RepositorioGenerico<Aluno, Integer> {
-
-    List<Aluno> listaDeAluno = new ArrayList<Aluno>();
+public class RepositorioAluno implements RepositorioGenerico{
 
     @Override
-    public void inserir(Aluno t) {
-        this.listaDeAluno.add(t);
+    public void inserir(Object t) {
+        DaoManagerHiber.getInstance().persist(t);
     }
 
     @Override
-    public void alterar(Aluno t) {
+    public void alterar(Object t) {
+        DaoManagerHiber.getInstance().update(t);
+    }
 
-        for (Aluno a : this.listaDeAluno) {
-            if (a.getCodigo() == t.getCodigo()) {
-                a.setCodigo(t.getCodigo());
-                a.setCpf(t.getCpf());
-                a.setIdade(t.getIdade());
-                a.setNome(t.getNome());
-            }
+    @Override
+    public Object recuperar(Object codigo) {
+        try {
+            return (Aluno) DaoManagerHiber.getInstance().recover("from Aluno where codigo" + codigo).get(0);
+        } catch (IndexOutOfBoundsException in) {
+            return null;
         }
     }
 
     @Override
-    public Aluno recuperar(Integer codigo) {
-
-        for (Aluno a : this.listaDeAluno) {
-            if (a.getNome().equals(codigo)) {
-                return a;
-            }
-        }
-        return null;
-
-    }
-
-    @Override
-    public void deletar(Aluno t) {
-        this.listaDeAluno.remove(t);
-
+    public void deletar(Object t) {
+        DaoManagerHiber.getInstance().delete(t);
     }
 
     @Override
     public List<Aluno> recuperarTodos() {
-
-        return this.listaDeAluno;
-
+        return DaoManagerHiber.getInstance().recover("from Aluno");
     }
-
+    
+    
+    
 }
